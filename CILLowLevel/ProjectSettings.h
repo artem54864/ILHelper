d3eb8292b4894740c493b7e3084e094dd595b0d6
unknown <Mykhaylo_Trusov@EPUAKHAW0643T1.kyiv.epam.com>
2015-02-16 17:37:03 +0200

    add new properties to ProjectSettings file; add unit tests.

--- CILLowLevel/ProjectSettings.h
+++ CILLowLevel/ProjectSettings.h
@@ -5,11 +5,16 @@ namespace CILLowLevel {
 
 	public ref class ProjectSettings
 	{
-		const String^ CONFIG_FILE = L".config";
+		const String^ CONFIG_FILE		= ".config";
+		const String^ ORDER_FILE		= ".order";
+		const String^ ASSEMBLY_FILE		= ".assembly";
+
+		const String^ CONTENT_FOLDER	= "content";
 
 		String^ _pathToProject;
 		String^ _projectName;
 		String^ _ilFileName;
+		String^ _contentFolder;
 
 	public:
 		ProjectSettings()
@@ -53,6 +58,33 @@ namespace CILLowLevel {
 			}
 		}
 
+		property const String^ ContentFolder {
+			const String^ get()
+			{
+				return CONTENT_FOLDER;
+			}
+		}
+
+		property const String^ ContentFolderPath {
+			const String^ get()
+			{
+				return _pathToProject + "\\" + ContentFolder;
+			}
+		}
+
+		property const String^ OrderFileName {
+			const String^ get()
+			{
+				return ORDER_FILE;
+			}
+		}
+
+		property const String^ AssemblyFileName {
+			const String^ get()
+			{
+				return ASSEMBLY_FILE;
+			}
+		}
 	};
 
 }
