414e6213fed364245a957a0082064dded369fa7d
michael-trusov <trusov.michael@gmail.com>
2015-02-17 00:11:50 +0200

    Add commands at CILAnalyzer

---
+++ CILAnalyzer/DisassemblePE.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "DisassemblePE.h"
+
+namespace cila
+{
+
+DisassemblePE::DisassemblePE()
+{
+}
+
+
+DisassemblePE::~DisassemblePE()
+{
+}
+
+void 
+DisassemblePE::execute()
+{
+}
+
+}
---
+++ CILAnalyzer/ImportCOMTypeLibraryToCILAssembly.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "ImportCOMTypeLibraryToCILAssembly.h"
+
+namespace cila
+{
+
+ImportCOMTypeLibraryToCILAssembly::ImportCOMTypeLibraryToCILAssembly()
+{
+}
+
+ImportCOMTypeLibraryToCILAssembly::~ImportCOMTypeLibraryToCILAssembly()
+{
+}
+
+void 
+ImportCOMTypeLibraryToCILAssembly::execute()
+{
+}
+
+}
