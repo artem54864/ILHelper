c93afb203c2c05d6c4c54cfa7fa1ef352bcce48d
Artem Beresniev <artem54864@gmail.com>
2015-02-17 20:11:05 +0300

    Settings. Added functionality, which allows loading and saving of application level settings (i.e. recent projects, etc.) to the predefined XML file.

---
+++ CILHelper/Settings/SettingsData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CILHelper.Settings
+{
+    /// <summary>
+    /// Instances of this class keep application level settings.
+    /// They contain some application level data (i.e. the list of recent projects, etc.), which shall be saved between solution runs in the "Settings" xml file.
+    /// The class is usually instantiated as a result of a deserialization and after the class instance is updated it is usually serialized back to the XML file.
+    /// </summary>
+    [Serializable]
+    [XmlRoot("Settings")]
+    public class SettingsData
+    {
+        /// <summary>
+        /// The list of paths (on the file system) to recent projects (the projects that a user worked with recently)
+        /// </summary>
+        [XmlArrayItem("path")]
+        public List<string> RecentProjects {get; set;}
+    }
+}
---
+++ CILHelper/Settings/SettingsProvider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CILHelper.Settings
+{
+    /// <summary>
+    /// The class is used to load and save Application Settings from \ to the predefined XML file.
+    /// </summary>
+    public class SettingsProvider
+    {
+        /// <summary>
+        /// The key is specified in the "App.config" file.
+        /// </summary>
+        private const string FilePathKey = "SettingsFilePath";
+
+        /// <summary>
+        /// The method loads settings from a persistent storage (the XML file)
+        /// and populates an instance of the "SettingsModel" class to be used in the application and returns it to a caller.
+        /// </summary>
+        /// <returns></returns>
+        public static SettingsData Load()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(SettingsData));
+            string settingsFilePath = ConfigurationManager.AppSettings[FilePathKey];
+            using (FileStream fs = new FileStream(settingsFilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return (SettingsData) serializer.Deserialize(fs);
+            }
+        }
+
+        /// <summary>
+        /// The method saves the settings from the "settingsToSave" argument to the persistant storage (the XML file)
+        /// </summary>
+        public static void Save(SettingsData settingsToSave)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(SettingsData));
+            string settingsFilePath = ConfigurationManager.AppSettings[FilePathKey];
+            using (FileStream fs = new FileStream(settingsFilePath, FileMode.Open, FileAccess.ReadWrite, FileShare.Read))
+            {
+                serializer.Serialize(fs, settingsToSave);
+            }
+        }
+    }
+}
