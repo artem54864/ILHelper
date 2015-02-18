7caf7655316b1c6622a862102cd82c38cd5717ee
Artem Beresniev <artem54864@gmail.com>
2015-02-18 14:15:40 +0300

    Settings A unit test has been added, which verifies "load \ save" settings functionality.

---
+++ Tests/Unit Tests/CILHelper_UnitTests/SettingsTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using CILHelper.Settings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CILHelper_UnitTests
+{
+    [TestClass]
+    public class SettingsTest
+    {
+        [TestMethod]
+        public void SaveLoadTest()
+        {
+            SettingsData settingsOld = SettingsProvider.Load();
+            int recentProjectsCountOld = settingsOld.RecentProjects.Count;
+            string randomNewRecentPrjectName = Guid.NewGuid().ToString();
+            settingsOld.RecentProjects.Add(randomNewRecentPrjectName);
+            SettingsProvider.Save(settingsOld);
+
+            SettingsData settingsUpdated = SettingsProvider.Load();
+
+            int currentRecentProjectsCount = settingsUpdated.RecentProjects.Count;
+
+            Assert.AreEqual(recentProjectsCountOld + 1, currentRecentProjectsCount);
+
+            Assert.AreEqual(settingsUpdated.RecentProjects[currentRecentProjectsCount - 1], randomNewRecentPrjectName);
+        }
+    }
+}
